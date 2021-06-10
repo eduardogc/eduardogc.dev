2a9eca772cd0272115d11be6759e990f87e38d73
Eduardo GC <eduardogc@MacBook-Air-de-Eduardo.local>
2021-06-10 08:41:44 -0300

    refactor css file, style tweak, host image from onedrive

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import '../styles/main.css'
-import '../styles/shorby-legacy.css'
+import '../styles/eduardogc-specific.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
