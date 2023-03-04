fa187cfaff07f45060f77e0da9b2bd76ce54e58d
eduardogc <eduardogc@outlook.com>
2023-03-04 19:22:03 -0300

    fix broken tailwind styles, update to new domain config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   purge: false,
   darkMode: 'class',
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {}
   },
